330ad83673e3fd9f2f88b7211ed1a6a356781764
James Halliday <mail@substack.net>
2014-09-05 00:36:28 -0700

    argv for the example add

--- example/add.js
+++ example/add.js
@@ -2,8 +2,5 @@ var batchdb = require('../');
 var db = require('level')('/tmp/compute.db');
 
 var compute = batchdb(db, { path: '/tmp/compute.blobs' });
-compute.add().end('sleep 5; date');
-
-compute.on('create', function (key) {
-    console.log('created', key);
-});
+var cmd = process.argv.slice(2).join(' ');
+compute.add().end(cmd);
